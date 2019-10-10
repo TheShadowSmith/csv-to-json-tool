aa7d0ed53d7fa753b67dc51e2f014a83a0f85954
Shadow Smith <hey@shadowsmith.com>
2019-10-10 14:48:43 -0500

    Add error handling for unsupported file types

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,26 @@ const router = express.Router()
 router.get('/', (req, res) => res.send('Hello from Get'))
 
 router.post('/', upload.single('file'), (req, res) => {
+
+    const fileName = req.file.originalname
+    const fileExtension = fileName.substr((fileName.lastIndexOf('.') + 1))
+    console.log(fileExtension)
+    if(fileExtension !== 'csv' && fileExtension !== 'txt') {
+        res.status(200).send({
+            error: {
+                message: 'Only CSV and txt files are accepted formats.'
+            }
+        })
+    }
     csv()
     .fromFile(req.file.path)
     .then(jsonObj => {
-        res.send(jsonObj)
-
         fs.unlinkSync(req.file.path, (err) => {
             if(err) {
                 throw new Error("Couldn't delete file")
             }
         })
+        res.status(200).send(jsonObj)
     })
 })
 
